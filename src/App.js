611a4838a4cc6fcb2758236961ca5e4316b1ba75
root <root@localhost.localdomain>
2020-11-14 16:11:45 +0200

    added travis deploy configfinalgood

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Pote tha xanafame spanakopita re Dimitri???</p>
+        <p>Pote tha xanafame spanakopita re Dimitri</p>
         <a
           className="App-link"
           href="https://reactjs.org"
